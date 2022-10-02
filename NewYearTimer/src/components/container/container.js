06531c9e35aa6c9bc94747d2d26a59bc0a3b1d18
LordFLader <devyansh1kalra@gmail.com>
2022-10-02 15:48:21 +0530

    added NewYearTimer

---
+++ NewYearTimer/src/components/container/container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./container.css";
+
+const container = () => {
+    return (
+        <div className='container'>
+            
+        </div>
+    );
+};
+
+export default container;
---
+++ NewYearTimer/src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./footer.css";
+
+const footer = () => {
+    return (
+        <div>
+            <h1 className='footer'>Made by Devyansh Kalra</h1>
+        </div>
+    );
+};
+
+export default footer;
